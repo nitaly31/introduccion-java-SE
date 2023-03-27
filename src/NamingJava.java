e582b4cd290f0ba48eb11a46161a0bf16019cab5
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-03-27 16:48:51 -0500

    Convencion de Nombres

---
+++ src/NamingJava.java
@@ -0,0 +1,24 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        //Convención de Nombres:
+
+        // Sensible a las mayusculas y minusculas
+        int celphone = 123456789;
+        int celPhone = 987654321;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        //Con simbolos
+        String $counttryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MX";
+        String background_Color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+
+    }
+}
