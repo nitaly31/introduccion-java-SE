003b67c43f31d5860a9a7fea455587092b2901e8
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-03-27 14:24:46 -0500

    practica con variables

---
+++ src/variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String[] args) {
+        int speed;
+        speed = 10;
+        System.out.println(speed);
+        int salary = 1000;
+        String employeeName = "gera";
+        System.out.println(employeeName);
+    }
+}
