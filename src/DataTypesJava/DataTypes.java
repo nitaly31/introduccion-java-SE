4705bfc835886d02035545630bf4d2f9144f2649
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-03-28 14:10:46 -0500

    Tipos de datos char y boolean

--- src/DataTypesJava/DataTypes.java
+++ src/DataTypesJava/DataTypes.java
@@ -5,13 +5,22 @@ public class DataTypes {
         //int es una variable que tiene un limite de espacio de memoria, pudiendo almacenar hasta 10 digitos.
         int n = 1234567890;
         // tipos de datos enteros: (byte = 1 byte) (short = 2 byte) (int = 4 bytes) (long = 8 bytes)
-
-        //En los casos de tipo long no solo basta iniciarlo con esa variable. sino tambien colocar la letra "L"
+        long nL = 12345678901L;//En los casos de tipo long no solo basta iniciarlo con esa variable. sino tambien colocar la letra "L"
         // al final del valor para que sea considerado de ese tipo.
-        long nL = 12345678901L;
         // Tipo de datos Flotantes: (float = 4 bytes) (double = 8 bytes)
         double nD = 1234.23456;
-        //para el caso de float se debe colocar la letra "F" al final del valor para poderlo diferenciar.
-        float nF = 1234.23456F;
+        float nF = 1234.23456F;//para el caso de float se debe colocar la letra "F" al final del valor para poderlo diferenciar.
+
+        var salary = 1000;//int
+        //pension 3%
+        var pension = salary*0.03;//double
+        var totalSalary = salary - pension; //double
+
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Geraldine Meneses";
+        System.out.println("EMPLOYEE: " + employeeName +" SALARY: " + salary);
     }
 }
